363b92abb7220f15a124db2d2fbef5eac0f88e56
QuinntyneBrown <quinntynebrown@gmail.com>
2021-12-29 08:49:24 -0500

    refactor

--- src/Combine.App/src/app/@core/combine.ts
+++ src/Combine.App/src/app/@core/combine.ts
@@ -1,16 +1,14 @@
 import { from, merge, Observable, ObservableInput, of } from "rxjs";
-import { map, scan } from "rxjs/operators";
+import { map, scan, startWith } from "rxjs/operators";
 
 export function combine(...sources: ObservableInput<any>[]): Observable<any> {
-
-  sources = sources.concat([of(true)]);
-
   return merge(
     ...(sources.map((source, index) => from(source).pipe(map((value) => ({ value, index })))
     ) as any)).pipe(
       scan((currentValues, {value, index}) => {
         currentValues[index] = value;
         return Array.from(currentValues);
-      }, new Array(sources.length))
+      }, new Array(sources.length)),
+      startWith([])
     );
 }
